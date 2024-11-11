982b83468d1a0d62b3b824c5ba169fa40a75e019
Foysal <mohibullah2k0@gmail.com>
2024-11-11 12:39:17 +0600

    login ui

---
+++ components/common/Logo.vue
@@ -0,0 +1,3 @@
+<template>
+    <img src="/images/apollo.png" alt="Logo" />
+  </template>
---
+++ pages/Register.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'identity',
+  })
+</script>
+
+<template>
+  <div
+    class="mt-10 overflow-hidden grid max-w-[980px] md:max-h-[600px] md:h-[100vh] mx-auto items-center  sm:grid-cols-1 md:grid-cols-2 mb-8 md:mb-0">
+    <FormRegister
+        class="p-6 md:p-12 border h-full border-gray-200 border-bottom-none md:border-bottomm md:border-right-none md:rounded-tl-md md:rounded-bl-md "/>
+    <BlocksToLogin />
+  </div>
+</template>
