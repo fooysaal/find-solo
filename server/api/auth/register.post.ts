a4e05e880769ca28d4ccce7533aa4ff4bf4f8586
Foysal <mohibullah2k0@gmail.com>
2024-11-13 15:20:31 +0600

    user registration

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,17 +1,35 @@
 // server/api/register.post.ts
-import { userRepository } from "~/server/repository/user";
+import {validateRegisterUser} from "~/server/utils/validators";
+import {userRepository} from "~/server/repository/User";
+import { PasswordHasher } from "~/server/providers/PasswordHasher";
 
 export default defineEventHandler(async (event) => {
+ const bodyData = await readBody(event); // 1
  
- const userData: any = {
-  email: "jonh.doe@gmail.com",
-  password: "12344",
-  name: "Jonh Doe",
- }
- 
- const result = userRepository.createUser(userData);
+ await validateRegisterUser(bodyData); // 2
 
+ const passwordHash = new PasswordHasher(10);
+ bodyData.password = await passwordHash.hash_password(bodyData.password);
+// creating user
+ await userRepository.createUser(bodyData); // 3
+
+//  // Getting request object
+// const req = event.node.req; // 1
+
+// // Get the host URL
+// const protocol = req.headers['x-forwarded-proto'] || 'http';
+// const host = req.headers.host;
+// const url = `${protocol}://${host}/auth/verify-email`;
+
+// // Generating token
+// const token = await tokenGenerator.generate(user, 
+//                 process.env.JWT_SECRET as string, {expiresIn: '1d'}); // 3
+
+// // Sending email verification
+// await mailer.sendEmail(bodyData.email, `${url}?token=${token}` );
+ 
  return {
-  data: result
+  status: 201,
+  message: "User registered successfully"
  }
 })
